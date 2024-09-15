aacfb1a3ac20826278867727ae87ea722d11deb2
DL6544 <DL6544@engie.com>
2024-09-16 01:10:36 +0200

    initial docs

--- main.ps1
+++ main.ps1
@@ -1,5 +1,20 @@
-# install package manager
+# install chocolatey package manager
 
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
+# close and reopen shell if needed
 
+choco install nmap `
+              git `
+              azure-cli `
+              terraform `
+              pycharm-community `
+              angryip `                 # fast port scans ?
+              unxutils `                # a bunch of common unix utils like grep etc (look into overwriting aliases)
+
+
+
+
+# once azure cli is installed use it to install kubectl
+
+# shell customizations with the startship and config file thing/
