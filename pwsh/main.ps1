40cf81aaf07808782bad22f5f5cd028d9355d826
DL6544 <DL6544@engie.com>
2024-09-30 01:28:36 +0200

    pwsh script

--- pwsh/main.ps1
+++ pwsh/main.ps1
@@ -23,26 +23,6 @@ if (!$chocoVersion) {
  Write-Output "Chocolatey is already installed. Version: $chocoVersion"
 }
 
-# install all the packages in the .env file, adjust as required
-Install-ChocoPackagesFromFile -packageFilePath "$ScriptPath\packages.env"
-
-# configure psreadline module for all users
-# https://www.powershellgallery.com/packages/PSReadLine/2.2.6
-Set-PSReadLineModule
-
-# Use azure cli to configure kubectl / kubelogin
-Install-Kubectl
-
-# 3. shell customizations with the startship and config file thing
-
-# 4. package with cicd pipeline into exe that can easily be ran as admin
-
-# 5. Add logging and error handeling.
-
-# function check to put in seperate module
-
-
-
 # Define an array of package names
 $packagesToInstall = @(
     "nmap",
@@ -77,7 +57,6 @@ function Install-ChocoPackagesFromFile {
 # Call the function and pass the array directly
 Install-ChocoPackagesFromFile -packagesToInstall $packagesToInstall
 
-
 function Set-PSReadLineModule {
     # Define the commands you want to append to the profile
 $commands = @"
@@ -92,7 +71,18 @@ $commands | Out-File -Append -FilePath $PROFILE.AllUsersAllHosts -Encoding utf8
 
 }
 
-function Install-Kubectl {
-    # using azure cli to auto configure kubectl / kubelogin etc
-    az aks install-cli
-}
+# configure psreadline module for all users
+# https://www.powershellgallery.com/packages/PSReadLine/2.2.6
+Set-PSReadLineModule
+
+
+# using azure cli to auto configure kubectl / kubelogin etc
+az aks install-cli
+
+# 3. shell customizations with the startship and config file thing
+
+# 4. package with cicd pipeline into exe that can easily be ran as admin
+
+# 5. Add logging and error handeling.
+
+# function check to put in seperate module
