c10ccf724422f2889d872140606f4fa69ab64f1d
DL6544 <DL6544@engie.com>
2024-09-29 20:29:00 +0200

    pwsh script

--- pwsh/main.ps1
+++ pwsh/main.ps1
@@ -1,7 +1,28 @@
-# install chocolatey package manager
+# -- Environment --
 
-# check before trying to install
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+# Set $ScriptPath to current directory compatible with PS2EXE
+if ($MyInvocation.MyCommand.CommandType -eq "ExternalScript") {
+    $ScriptPath = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
+}
+else {
+    $ScriptPath = Split-Path -Parent -Path ([Environment]::GetCommandLineArgs()[0])
+    if (!$ScriptPath) {
+        $ScriptPath = "."
+    }
+}
+
+# Import the required Functions from src
+Import-Module -Name "$scriptPath\src\Functions\Functions.psd1"
+
+# Install Chocolatey
+# TODO: check before trying to install
+ $chocoVersion = choco --version 2>$null
+ if (!$chocoVersion) {
+     Write-Output "Chocolatey will be installed"
+     Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+ } else {
+     Write-Output "Chocolatey is already installed. Version: $chocoVersion"
+ }
 
 # close and reopen shell if needed
 
@@ -12,6 +33,8 @@ Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManage
 # 3. shell customizations with the startship and config file thing
 
 # 4. package with cicd pipeline into exe that can easily be ran as admin
+
+# 5. Add logging and error handeling.
 
 # function check to put in seperate module
 
