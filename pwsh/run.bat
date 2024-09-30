d4589e6cd25a1f0aaa56d824375eb6c8b6cf526c
DL6544 <DL6544@engie.com>
2024-09-30 02:59:43 +0200

    pwsh script

--- pwsh/run.bat
+++ pwsh/run.bat
@@ -1,3 +1,3 @@
 @echo off
-PowerShell -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-ExecutionPolicy Bypass -File "".\main.ps1""' -Verb RunAs}"
+PowerShell -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-ExecutionPolicy Bypass -File ""%~dp0main.ps1""' -Verb RunAs}"
 pause
