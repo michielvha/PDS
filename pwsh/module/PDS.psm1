3489834e86d19930d94419074659d55a6b3cc1d2
DL6544 <DL6544@engie.com>
2024-10-23 20:34:36 +0200

    adding recurse so get-childitem also looks in sub directories for new function files, in case I want to start spliting them into subdirs for easier overview.

--- pwsh/module/PDS.psm1
+++ pwsh/module/PDS.psm1
@@ -1,4 +1,4 @@
-# Import all functions from the Public folder
+# Import all functions from the Public folder and sub folders
 Get-ChildItem -Path (Join-Path $PSScriptRoot 'Public') -Filter *.ps1 -Recurse | ForEach-Object {
     . $_.FullName
 }
