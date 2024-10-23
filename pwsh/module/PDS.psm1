fb753aaee610045eb6f460c503d47e71c2e60f2a
DL6544 <DL6544@engie.com>
2024-10-23 15:44:46 +0200

    update module

--- pwsh/module/PDS.psm1
+++ pwsh/module/PDS.psm1
@@ -1,7 +1,7 @@
 # pds.psm1
 
 # Import all functions from the Public folder
-Get-ChildItem -Path (Join-Path $PSScriptRoot 'Public') | ForEach-Object {
+Get-ChildItem -Path (Join-Path $PSScriptRoot 'Public') -Filter *.ps1 | ForEach-Object {
     . $_.FullName
 }
 
