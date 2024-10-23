cfe3d79d1a0e3069c88a65b2169c97882d019ed2
DL6544 <DL6544@engie.com>
2024-10-23 19:11:16 +0200

    creating Enable-BGInfoAtStartup function

--- pwsh/module/Public/Enable-BGInfoAtStartup.ps1
+++ pwsh/module/Public/Enable-BGInfoAtStartup.ps1
@@ -1,15 +1,18 @@
-# Path to the .bgi file inside the assets folder of the module
-$bgiFilePath = Join-Path $PSScriptRoot "assets\default.bgi"
-$bginfoPath = "C:\ProgramData\chocolatey\lib\bginfo\tools\Bginfo64.exe"
-$arguments = "`"$bgiFilePath`" /timer:0"
+Function Enable-BGInfoAtStartup {
+    # Path to the .bgi file inside the assets folder of the module
+    $bgiFilePath = Join-Path $PSScriptRoot "assets\default.bgi"
+    $bginfoPath = "C:\ProgramData\chocolatey\lib\bginfo\tools\Bginfo64.exe"
+    $arguments = "`"$bgiFilePath`" /timer:0"
 
-# Create a shortcut in the startup folder
-$shortcutPath = Join-Path "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup" "BGInfo.lnk"
-$WshShell = New-Object -ComObject WScript.Shell
-$shortcut = $WshShell.CreateShortcut($shortcutPath)
-$shortcut.TargetPath = $bginfoPath
-$shortcut.Arguments = $arguments
-$shortcut.Save()
+    # Create a shortcut in the startup folder
+    $shortcutPath = Join-Path "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup" "BGInfo.lnk"
+    $WshShell = New-Object -ComObject WScript.Shell
+    $shortcut = $WshShell.CreateShortcut($shortcutPath)
+    $shortcut.TargetPath = $bginfoPath
+    $shortcut.Arguments = $arguments
+    $shortcut.Save()
+}
+
 
 # Optional: You can also add a scheduled task to run BGInfo if preferred (not tested - I've done this before somewhere, go look if needed.)
 #$action = New-ScheduledTaskAction -Execute "C:\ProgramData\chocolatey\lib\bginfo\tools\Bginfo64.exe" -Argument '"C:\path\to\bginfo.bgi" /silent /timer:0'
