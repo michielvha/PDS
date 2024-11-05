84921ed46a753c1a0e81509d96665ecd565456c6
DL6544 <DL6544@engie.com>
2024-11-05 21:49:05 +0100

    Adding new function: Install-ArgoCLI

--- pwsh/module/Public/Install-ArgoCLI.ps1
+++ pwsh/module/Public/Install-ArgoCLI.ps1
@@ -1,24 +1,24 @@
 function Install-ArgoCLI {
 <#
 .SYNOPSIS
-    Installs Chocolatey if it is not already installed on the system.
+    Installs ArgoCD CLI if it is not already installed on the system.
 
 .DESCRIPTION
-    The `Install-Choco` function checks if Chocolatey is installed on the system by attempting to retrieve its version. If Chocolatey is not installed, the function will download and execute the official installation script from the Chocolatey website. If Chocolatey is already installed, it outputs the current version.
+    The `Install-ArgoCLI` function checks if ArgoCD CLI is installed on the system by attempting to retrieve its version. If ArgoCD CLI is not installed, the function will download and execute the official installation script from the ArgoCD CLI website. If ArgoCD CLI is already installed, it outputs the current version.
 
 .EXAMPLE
-    Install-Choco
+    Install-ArgoCLI
 
-    This example checks if Chocolatey is installed on the system. If it is not, the function installs Chocolatey. If it is already installed, it prints the installed version of Chocolatey.
+    This example checks if ArgoCD CLI is installed on the system. If it is not, the function installs ArgoCD CLI. If it is already installed, it prints the installed version of ArgoCD CLI.
 
 .NOTES
-    Author: MKTHEPLUGG
-    Requires: Internet connection for downloading Chocolatey if it is not already installed.
-    This function modifies the execution policy temporarily to allow Chocolatey to be installed.
+    Author: itmvha
+    Requires: Internet connection for downloading ArgoCD CLI if it is not already installed.
+
 
 .LINK
-    https://chocolatey.org/
-    Learn more about Chocolatey and how it is used for package management.
+    https://argocd-readthedocs.io/
+    Learn more about ArgoCD CLI and how it is used for package management.
 #>
 
 # 1. Check if installed
