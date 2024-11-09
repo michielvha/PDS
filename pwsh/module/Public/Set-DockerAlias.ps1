81680e8c417399a3297156f83d511dea150f0507
DL6544 <DL6544@engie.com>
2024-11-09 22:27:33 +0100

    Adding new function: Set-DockerAlias

--- pwsh/module/Public/Set-DockerAlias.ps1
+++ pwsh/module/Public/Set-DockerAlias.ps1
@@ -7,7 +7,7 @@ function Set-DockerAlias {
     The `Set-DockerAlias` function sets aliases for common Docker commands to simplify their usage. The function checks if the aliases are already set and only sets them if they are not defined.
 
     The aliases set by this function are:
-    - `dc` for `docker compose up -d`
+    - `dcu` for `docker compose up -d`
     - `dc` for `docker compose`
     - `dcd` for `docker compose down`
 
@@ -19,10 +19,10 @@ function Set-DockerAlias {
 
 
 $aliases =  @(
-        @{Name="dc"; Command="docker compose up -d"}
-        @{Name="dc"; Command="docker compose"}
-        @{Name="dcd"; Command="docker compose down"}
-        )
+                @{Name="dc"; Command="docker compose"}
+                @{Name="dcu"; Command="docker compose up -d"}
+                @{Name="dcd"; Command="docker compose down"}
+            )
 
     foreach ($alias in $aliases) {
         $aliasName = $alias.Name
