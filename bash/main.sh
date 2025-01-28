0fd175887972b1d65241c2c4b167598963876894
Mike <michielvh@outlook.com>
2025-01-28 03:28:49 +0100

    some bash functions

--- bash/main.sh
+++ bash/main.sh
@@ -4,7 +4,23 @@
 
 # install software
 
+# ------------------------------------------------------------------------------------------------------------------------------------------------
+
 # Configure ZSH
+function Install_ZSH() {
+# Set the default shell to zsh
+sudo chsh -s $(which zsh) $(whoami)
+# Install oh-my-zsh: https://ohmyz.sh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# Install Powerlevel10k: https://github.com/romkatv/powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+sudo grep -q "ZSH_THEME="powerlevel10k/powerlevel10k"" ~/.zshrc || \
+echo "ZSH_THEME="powerlevel10k/powerlevel10k"" | sudo tee -a ~/.zshrc > /dev/null
+# find some way to automate the configuration of powerlevel10k by copying the .p10k.zsh file from the repo to the home directory
+}
+
+# ------------------------------------------------------------------------------------------------------------------------------------------------
 
 # Function: Create system-wide Crontab to auto update system every night at midnight.
 # first check with grep to see if entry doesn't already exist. We use logical || (or) operator to only append if the grep command returns false(1). if it returns true(0) it means the entry already exists and no action will be taken.
@@ -14,6 +30,8 @@ function update_system_cron_entry() {
 }
 
 update_system_cron_entry
+
+# ------------------------------------------------------------------------------------------------------------------------------------------------
 
 # Function: full upgrade with one command
 
