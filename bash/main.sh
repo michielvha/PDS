741a77c6da3567c2fa3283a59116593e2ded6110
Mike <michielvh@outlook.com>
2025-01-28 18:41:12 +0100

    install zsh and docs

--- bash/main.sh
+++ bash/main.sh
@@ -8,6 +8,8 @@
 
 # Configure ZSH
 function Install_ZSH() {
+# install dependencies
+sudo apt install zsh git curl -y
 # Set the default shell to zsh
 sudo chsh -s $(which zsh) $(whoami)
 # Install oh-my-zsh: https://ohmyz.sh
@@ -15,9 +17,28 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 # Install Powerlevel10k: https://github.com/romkatv/powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
-sudo grep -q "ZSH_THEME="powerlevel10k/powerlevel10k"" ~/.zshrc || \
-echo "ZSH_THEME="powerlevel10k/powerlevel10k"" | sudo tee -a ~/.zshrc > /dev/null
+if grep -q '^ZSH_THEME=' ~/.zshrc; then
+    # Replace the existing line
+    sudo sed -i 's|^ZSH_THEME=.*|ZSH_THEME="powerlevel10k/powerlevel10k"|' ~/.zshrc
+else
+    # Add the line if it doesn't exist
+    echo 'ZSH_THEME="powerlevel10k/powerlevel10k"' | sudo tee -a ~/.zshrc > /dev/null
+fi
+
+# Enable Extensions: https://github.com/ohmyzsh/ohmyzsh/wiki/Plugins
+# Define the list of plugins
+PLUGINS_LIST="git history z docker"
+# Update PLUGINS in ~/.zshrc
+if grep -q '^plugins=' ~/.zshrc; then
+    # Replace the existing plugins line
+    sudo sed -i "s|^plugins=.*|plugins=($PLUGINS_LIST)|" ~/.zshrc
+else
+    # Add the plugins line if it doesn't exist
+    echo "plugins=($PLUGINS_LIST)" | sudo tee -a ~/.zshrc > /dev/null
+fi
+
 # find some way to automate the configuration of powerlevel10k by copying the .p10k.zsh file from the repo to the home directory
+source ~/.zshrc
 }
 
 # ------------------------------------------------------------------------------------------------------------------------------------------------
