81f5cf84f4dd5be2b521cd45a3b85494f88db505
Mike <michielvh@outlook.com>
2025-01-28 01:43:09 +0100

    bash functions

--- bash/main.sh
+++ bash/main.sh
@@ -5,3 +5,12 @@
 # install software
 
 # Configure ZSH
+
+# Function: Create system-wide Crontab to auto update system every night at midnight.
+# first check with grep to see if entry doesn't already exist. We use logical || (or) operator to only append if the grep command returns false(1). if it returns true(0) it means the entry already exists and no action will be taken.
+function update_system_cron_entry() {
+    sudo grep -q "apt update -y && apt upgrade -y" /etc/crontab || \
+    echo "0 0 * * * root apt update -y && apt upgrade -y" | sudo tee -a /etc/crontab > /dev/null
+}
+
+update_system_cron_entry
