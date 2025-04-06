1385de14c1c5f1b24f71f348027740a66ce85bf8
michielvha <michielvh@outlook.com>
2025-04-06 13:51:35 +0200

    feat: added ZSH setup + added various tools

--- darwin/install.sh
+++ darwin/install.sh
@@ -1,20 +1,82 @@
-# How i setup my macbook
+# My macbook setup
 
 # Install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 echo >> /Users/michielvh/.zprofile\
     echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/michielvh/.zprofile\
     eval "$(/opt/homebrew/bin/brew shellenv)"
-# Install CLI Homebrew packages
-brew install arc warp iterm2 gh
-
 
 # Install Xcode command line tools
 # This is required for Homebrew to work properly
 xcode-select --install
 
-# install vscode
-brew install --cask visual-studio-code
+# Install CLI Homebrew packages
+brew install arc \
+     warp \
+     iterm2 \
+     gh \
+     go \
+     neofetch \
+     kubectl \
+     helm \
+     kustomize
 
-# Install chatGPT
-brew install --cask chatgpt
+
+# Install GUI Homebrew packages
+# make sure the font is installed: https://github.com/romkatv/powerlevel10k?tab=readme-ov-file#manual-font-installation
+brew install --cask visual-studio-code \
+     chatgpt
+
+install_zi (){
+    sh -c "$(curl -fsSL https://git.io/zi-install)"
+}
+
+configure_zsh (){
+    cat <<EOF | sudo tee ~/.zshrc
+source ~/.zi/bin/zi.zsh
+zi light romkatv/powerlevel10k
+
+### --- Plugins ---
+zi light zsh-users/zsh-syntax-highlighting
+zi light zsh-users/zsh-autosuggestions
+zi light zsh-users/zsh-history-substring-search
+zi light agkozak/zsh-z
+
+# Installs stuff in ~/zi/plugins that needs to be manually sourced.
+zi light ohmyzsh/ohmyzsh
+zi snippet OMZP::docker
+zi snippet OMZP::history
+source "$HOME/.zi/plugins/ohmyzsh---ohmyzsh/plugins/git/git.plugin.zsh"
+
+zi light marlonrichert/zsh-autocomplete
+zstyle ':autocomplete:*' default-context history-incremental-search-backward
+zstyle ':autocomplete:*' min-input 1
+setopt HIST_FIND_NO_DUPS
+
+# --- Enable kubectl autocompletion for zsh ---
+#autoload -Uz compinit && compinit # zi enables this automatically.
+source <(kubectl completion zsh)
+
+# alias + auto complete for alias
+alias k='kubectl'
+compdef __start_kubectl k
+
+# --- Various ---
+neofetch
+alias knr='kubectl get pods --field-selector=status.phase!=Running'
+EOF
+}
+
+setup_zsh (){
+    # Install zsh
+    brew install zsh
+
+    # Set zsh as default shell
+    chsh -s $(which zsh)
+
+    # Install zi
+    install_zi
+
+    # Configure zsh
+    configure_zsh
+}
--- darwin/readme.md
+++ darwin/readme.md
@@ -10,3 +10,18 @@ placeholder for when i switch to macOS
 
 - `command + q` - actually quit apps (red cross doesn't behave like on windows).
 - ` Ctrl + `` (backtick) ` -  quickly open a terminal in VSCode.
+
+
+- `Option (⌥) + Shift (⇧) + /` - Backslash: **\\**
+-	`Option (⌥) + Shift (⇧) + L` - Pipe: **|**
+-	`Option (⌥) + Shift (⇧) + (` - Left Square Bracket: **[**
+-	`Option (⌥) + Shift (⇧) + )`- Right Square Bracket: **]**
+-	`Option (⌥) + (` - Left Curly Brace: **{**
+-	`Option (⌥) + )` - Right Curly Brace: **}**
+
+
+## ZSH Setup
+
+install the [font](https://github.com/romkatv/powerlevel10k?tab=readme-ov-file#manual-font-installation).
+
+run the function `setup_zsh`
