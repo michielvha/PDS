907430f6e17b62299d45eefc111fbb91a5d8beaf
michielvha <michielvh@outlook.com>
2025-06-02 10:46:49 +0200

    fixes

--- bash/debian/ui/set_gnome_fonts.sh
+++ bash/debian/ui/set_gnome_fonts.sh
@@ -6,9 +6,13 @@
 set_gnome_fonts() {
 	echo "🎨 Setting GNOME fonts and rendering..."
 
-	gsettings set org.gnome.desktop.interface font-name 'JetBrains Mono 11'
-	gsettings set org.gnome.desktop.interface document-font-name 'JetBrains Mono 11'
-	gsettings set org.gnome.desktop.interface monospace-font-name 'JetBrains Mono 13'
+	# gsettings set org.gnome.desktop.interface font-name 'JetBrains Mono 11'
+	# gsettings set org.gnome.desktop.interface document-font-name 'JetBrains Mono 11'
+	# gsettings set org.gnome.desktop.interface monospace-font-name 'JetBrains Mono 13'
+
+	gsettings set org.gnome.desktop.interface font-name 'Noto Sans Regular 11'
+	gsettings set org.gnome.desktop.interface document-font-name 'Noto Sans Regular 11'
+	gsettings set org.gnome.desktop.interface monospace-font-name 'Noto Sans Mono Regular 13'
 
 	echo "✅ Font configuration applied."
 }
