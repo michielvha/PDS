c9746f3bea180d1321fc7a1d7a04b1d452c9fa7e
michielvha <michielvh@outlook.com>
2025-07-17 21:09:55 +0200

    feat: create admin user

--- bash/common/shell/create_admin.sh
+++ bash/common/shell/create_admin.sh
@@ -1,18 +1,34 @@
+#!/bin/bash
+# Source: ` source <(curl -fsSL https://raw.githubusercontent.com/michielvha/PDS/main/bash/common/shell/set_sudo_nopasswd.sh) `
+
+# Function: set_sudo_nopasswd
+# Description: Setup passwordless sudo for a user
 configure_admin (){
   local SSH_PUBLIC_KEY="ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJuz/p0uJEULyptuR7US4GnGmCziaKLQsxYO5VyAx+Oa sysadmin@mvha.eu.org"
+  local ADMIN="$1"
+
+  if [[ -z "$ADMIN" ]]; then
+    echo "Usage: configure_admin <username>"
+    return 1
+  fi
+
   # TODO: Check if user already exists before creating
   # Create the user and set the public key for SSH authentication
-  echo "==> Creating user and setting up SSH public key..."
-  sudo useradd -m -s /bin/bash "$USER_NAME" #TODO: change to zsh ?
-  sudo mkdir -p /home/"$USER_NAME"/.ssh
-  echo "$SSH_PUBLIC_KEY" | sudo tee /home/"$USER_NAME"/.ssh/authorized_keys
-  sudo chown -R "$USER_NAME":"$USER_NAME" /home/"$USER_NAME"/.ssh
-  sudo chmod 700 /home/"$USER_NAME"/.ssh
-  sudo chmod 600 /home/"$USER_NAME"/.ssh/authorized_keys
+  echo "👤 Creating admin user and setting up SSH public key..."
+  sudo useradd -m -s /bin/bash "$ADMIN" #TODO: change to zsh ?
+  sudo mkdir -p /home/"$ADMIN"/.ssh
+
+  echo "$SSH_PUBLIC_KEY" | sudo tee /home/"$ADMIN"/.ssh/authorized_keys
+  sudo chown -R "$ADMIN":"$ADMIN" /home/"$ADMIN"/.ssh
+  sudo chmod 700 /home/"$ADMIN"/.ssh
+  sudo chmod 600 /home/"$ADMIN"/.ssh/authorized_keys
+
   # Add the user to the sudo group
-  echo "==> Adding $USER_NAME to the sudo group..."
-  sudo usermod -aG sudo "$USER_NAME"
-  echo "==> Adding sourcing of pds main functions for $USER_NAME."
-  echo "source <(curl -fsSL https://raw.githubusercontent.com/michielvha/PDS/main/bash/module/install.sh)" >> /home/"$USER_NAME"/.bashrc
-  echo "User $USER_NAME created, SSH key added, user added to sudo group and bashrc modified."
+  echo "🔐 Adding $ADMIN to the sudo group..."
+  sudo usermod -aG sudo "$ADMIN"
+
+  # Configure .bashrc
+  # echo "🔧 Adding sourcing of pds main functions for $ADMIN."
+  # echo "source <(curl -fsSL https://raw.githubusercontent.com/michielvha/PDS/main/bash/module/install.sh)" >> /home/"$ADMIN"/.bashrc
+  # echo "User $ADMIN created, SSH key added, user added to sudo group and bashrc modified."
 }
