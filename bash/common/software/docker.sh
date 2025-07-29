f488a94f2acb7c307a1f691378bf3ca89270fadf
michielvha <105451808+michielvha@users.noreply.github.com>
2025-07-30 01:30:14 +0200

    Update docker.sh

--- bash/common/software/docker.sh
+++ bash/common/software/docker.sh
@@ -1,4 +1,9 @@
+#!/bin/bash
+# shellcheck disable=SC2016
+# Source: ` source <(curl -fsSL https://raw.githubusercontent.com/michielvha/PDS/main/bash/common/software/docker.sh) `
 
+# Function: refresh_docker_compose
+# Description: Remove a container tagged with latest to be sure the container is repulled and relaunch the docker compse stack.
 refresh_docker_compose(){
 
   # docker compose down
