73d135b1adc03a194f893c7bb8e95238f02a647f
michielvha <michielvh@outlook.com>
2026-01-03 20:26:38 +0100

    chore: update system bootstrap script to include additional packages and enable Docker
    - Added net-tools, bat, btop, fastfetch, and yq to the installation command for enhanced functionality.
    - Enabled and started Docker service, and added the user to the Docker group for access.

--- bash/arch/main.sh
+++ bash/arch/main.sh
@@ -1,14 +1,15 @@
-sudo pacman -S zsh firefox git go nvm 
+sudo pacman -S zsh firefox git go nvm net-tools bat btop fastfetch yq make docker docker-compose
 
 # Add nvm to profile
 source /usr/share/nvm/init-nvm.sh >> ~/.bashrc
 source /usr/share/nvm/init-nvm.sh >> ~/.zshrc
 
-sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si
-
-yay cursor-bin
-
-sudo pacman -S docker docker-compose
+# enable docker
 sudo systemctl enable docker.service
 sudo systemctl start docker.service
 sudo usermod -aG docker $USER
+
+# Install yay (AUR helper)
+sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si
+
+yay cursor-bin
